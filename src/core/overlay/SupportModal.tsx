85236a47cda81fb87d62021fc5d0a4d59ed19656
Enmanuel Antoni Bracho Villanueva <enmanuel02estudios@gmail.com>
2025-12-30 14:56:43 -0400

    chore: Update src/core/overlay/SupportModal.tsx

--- src/core/overlay/SupportModal.tsx
+++ src/core/overlay/SupportModal.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useForm, useWatch } from "react-hook-form";
 
-import { Modal } from "@core/overlay/Modal";
+import Modal from "@core/overlay/Modal";
 import { Button } from "@core/ui/Button";
 
 interface Props {
