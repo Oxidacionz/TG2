2c0be289238f05bce88b5fe732fd2073fad24064
Enmanuel Antoni Bracho Villanueva <enmanuel02estudios@gmail.com>
2025-12-30 14:56:30 -0400

    chore: Update src/core/form/Input.tsx

--- src/core/form/Input.tsx
+++ src/core/form/Input.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import { forwardRef, InputHTMLAttributes } from "react";
 
-export const Input = React.memo(
-  React.forwardRef<
-    HTMLInputElement,
-    React.InputHTMLAttributes<HTMLInputElement>
-  >((props, ref) => {
-    const { className, ...rest } = props;
-    return (
-      <input
-        ref={ref}
-        className={`focus:ring-brand-500 w-full rounded-md border border-slate-300 bg-white px-3 py-2 text-sm text-slate-900 placeholder-slate-400 transition-shadow focus:border-transparent focus:ring-2 focus:outline-none dark:border-slate-700 dark:bg-slate-800 dark:text-white ${className}`}
-        {...rest}
-      />
-    );
-  }),
-);
-Input.displayName = "Input";
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  controlsNumber?: boolean;
+}
+
+const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { controlsNumber = false, className = "", ...props },
+  ref,
+) {
+  const styles =
+    "focus:ring-brand-500 w-full rounded-md border border-slate-300 bg-white px-3 py-2 text-sm text-slate-900 placeholder-slate-400 transition-shadow focus:border-transparent focus:ring-2 focus:outline-none focus:ring-inset dark:border-slate-700 dark:bg-slate-800 dark:text-white";
+
+  const noControlsStyles =
+    "[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none";
+
+  const shouldHideControls = props.type === "number" && !controlsNumber;
+
+  return (
+    <input
+      ref={ref}
+      className={`${styles} ${shouldHideControls ? noControlsStyles : ""} ${className}`}
+      {...props}
+    />
+  );
+});
+
+export default Input;
